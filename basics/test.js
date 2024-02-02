a870efb53c90e8a05835ec501a69ee816e38f095
rashmi mishra <154453212+rashmimisra@users.noreply.github.com>
2024-02-02 18:28:02 +0000

    datatypes add

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,25 @@
 console.log("rashmi")
+const accountid = 1234
+let accountemail="shiva123@gmail.com"
+var accountname="534hdfc"
+city = "jaipur"
+let accountstate ;// undefined state
+console.table([accountid , accountemail , accountname , city, accountstate])
+/* data types 
+primitive data types
+let 
+boolean => true / false 
+symbol => uniqueness (important )
+string => '' or ""
+undefined => no value is assinged to a variable ex : let salary ;
+null => kisi conditionn ko show karne ke liye use hota hai like temperatre if not recived then show null because 0 is also temperature
+typeof => kisi bhi data type ka type janna ho tab use karte hai 
+typeof null => object 
+typeof undefined=> undefined 
+ye dono special case hai 
+*/
+console.log(typeof null)
+console.log(undefined)
+
+
+
