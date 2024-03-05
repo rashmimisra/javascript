ca2b660a58425aafe40e4849a5b751dcdf3c730b
rashmi mishra <154453212+rashmimisra@users.noreply.github.com>
2024-03-05 14:34:17 +0000

    coversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -14,9 +14,15 @@ let r = "rashmi " ;
 let bool = Boolean( r);
 console.log ( bool)
 // if r= "" || undefined || null ;  => bollean value false 
+
 /********************************************************operations ****************************************************/
 console.log ( +true) // output 1 but true+ is an error 
 console.log("1"+ 2) // treateed as string and output will be 12
 console.log( 1+ "2 ") // will also be treated as string and the output will be 12
 console.log("1" + 2 + 2)// will also be trated as string and the output will be 122 since 1 is string thus after that everything will be treated as string 
 console.log( 1+ 2 + "2") // here the output will be 32 as the string  is written at last thus the previous values will be treated as string and the normal operations will be performed.
+
+/********************************conversions **********************************/
+console.log(null > 0);
+console.log(null == 0)
+console.log (null >= 0)// here the output is true because null is converted into 0 then compared while in the above ones the the null is not converted best thing is to avoid these type of conversions
