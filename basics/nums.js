35e79ce13e7ef7a5d6cd98039aa18de2a0ff11a7
rashmi mishra <154453212+rashmimisra@users.noreply.github.com>
2024-04-03 18:32:38 +0000

    numbers

---
+++ basics/nums.js
@@ -0,0 +1,12 @@
+const score =400;
+const balance= new Number(100)
+console.log(score);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));// fixes the length of a number to be displayed after decimal
+const othernumber= 123.4463
+console.log(othernumber.toPrecision(3));
+const hundred=100000
+const num=123456789
+console.log(hundred.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
+console.log(num.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
