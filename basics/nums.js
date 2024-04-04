09680c38b486fa368e8e04de18ad26aa3fd53694
rashmi mishra <154453212+rashmimisra@users.noreply.github.com>
2024-04-04 17:41:53 +0000

    maths

--- basics/nums.js
+++ basics/nums.js
@@ -1,12 +1,31 @@
-const score =400;
-const balance= new Number(100)
-console.log(score);
-console.log(balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));// fixes the length of a number to be displayed after decimal
-const othernumber= 123.4463
-console.log(othernumber.toPrecision(3));
-const hundred=100000
-const num=123456789
-console.log(hundred.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
-console.log(num.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
+//const score =400;
+//const balance= new Number(100)
+//console.log(score);
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2)); // fixes the length of a number to be displayed after decimal
+//const othernumber= 123.4463
+//console.log(othernumber.toPrecision(3));
+//const hundred=100000
+//const num=123456789
+//console.log(hundred.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
+//console.log(num.toLocaleString('en-IN'));// this method is used to display the numbers with the commas and en-in is used to represent it in indian system
+
+
+/////+++++++++++++++++++++++++++++++maths+++++++++++++++++++++/
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(1.533));
+console.log(Math.ceil(1.324));
+console.log(Math.floor(112.34532));
+const min=10;   // simply definig the variables min and max
+const max=20;
+console.log(Math.random()); 
+console.log(Math.floor(Math.random()* (max-min +1))+min);// here random method will give the value between 0 and 1 but to get the values other than one we can write random()*10
+// but to avoid the condition of zero we add 1 in it so we don't get the value zero . here we have defined the max and min values  to get the min value we made this formula and just rememmber it
+
+
+
+
+
+
